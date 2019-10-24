9fcbbdfb11ea95834ef05ac463ff2d7dacc6f379
kevin balboa <kpbalboa@gmail.com>
2019-10-24 15:18:43 -0400

    initial commit display card images

---
+++ src/card.js
@@ -0,0 +1,11 @@
+// function card(props){
+    
+//     return (
+//         <button>
+//            <img src={props.img}></img>
+//            </button>
+//     )
+ 
+//     }
+
+//     export default card;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import './img'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const cardsArray = [
+    {
+      name: "mushroom",
+      img: "img/mushroom.png"
+    },
+    {
+      name: "Flower",
+      img: "img/flower.png"
+    },
+    {
+      name: "Star",
+      img: "img/Star.png"
+    },
+    {
+      name: "coin",
+      img: "img/coin.png"
+    },
+    {
+      name: "leaf",
+      img: "img/leaf.png"
+    },
+    {
+      name: "1Up",
+      img: "img/_1Up.png"
+    },
+    {
+      name: "bell",
+      img: "img/bell.png"
+    },
+    {
+      name: "Shine",
+      img: "img/shine.png"
+    },
+    {
+      name: "shell",
+      img: "img/shell.png"
+    }
+  ];
+  const gameGrid = cardsArray.concat(cardsArray);
+  gameGrid.sort(() => 0.5 - Math.random());
+const cards = gameGrid.map((card)=>
+<img src={require (`./${card.img}`)}></img>
+);
+ReactDOM.render(
+    <div>
+    
+       <ul>{cards}</ul>
+    </div>,
+    document.getElementById('root')
+  );
+  
